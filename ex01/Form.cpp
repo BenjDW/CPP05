709aa804021ecb71529fd5bb1e0c597d5af67f28
BenjDW <dewitskibenji@live.fr>
2025-02-11 07:46:24 -0800

    need to debug but without .h problem

---
+++ ex01/Form.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 06:09:16 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/02/11 07:35:29 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+std::ostream&	operator<<(std::ostream& out, Form& f)
+{
+	out << f.getName() << " Form , Form grade " << f.getGrade() << ", Form grade execution" << "" << std::endl;
+	return (out);
+}
+
+void	Form::beSigned(Bureaucrat& agent)
+{
+	if (agent.getGrade() < 1)
+		throw(GradeTooHighException());
+	else if (agent.getGrade() > 150 || agent.getGrade() > this->grade)
+		throw(GradeTooLowException());
+	else if (this->signe == false)
+		this->signe = true;
+	else
+		throw(AlreadySignedException());
+}
+
+bool	Form::getSigne()
+{
+	return (this->signe);
+}
+
+std::string	Form::getName()
+{
+	return (this->name);
+}
+
+const int32_t	Form::getGrade_exec()
+{
+	return (this->grade_exec);
+}
+
+const int32_t	Form::getGrade()
+{
+	return (this->grade);
+}
+
+Form::Form() : name ("Triple The Tax"), signe (false), grade (11), grade_exec (12)
+{
+}
+
+Form::Form(const std::string named) : signe (false), grade (11), grade_exec (12)
+{
+	this->name = named;
+}
+
+Form::Form(Form &cpy) : signe (cpy.signe), grade(cpy.grade), grade_exec (cpy.grade_exec), name (cpy.name)
+{
+	// this->name = cpy.name;
+}
+
+Form::~Form()
+{
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 08:32:11 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/02/11 07:32:18 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int	main()
+{
+	try 
+	{
+        Bureaucrat	b(151); // Trop bas
+    } 
+	catch (const std::exception &e)
+	{
+        std::cerr << "Erreur : " << e.what() << std::endl;
+    }
+
+    try 
+	{
+        Bureaucrat b(0); // Trop haut
+    }
+	catch (const std::exception &e)
+	{
+        std::cerr << "Erreur : " << e.what() << std::endl;
+    }
+
+    try
+	{
+        Bureaucrat b(1);
+		std::cout << b;
+        b.incremental_grade(); // Trop haut après incrément
+    } 
+	catch (const std::exception &e)
+	{
+        std::cerr << "Erreur : " << e.what() << std::endl;
+    }
+
+    return 0;
+}
